81cfca7fe932b5b2cc705bfc941ec42a9d383de2
doantrang9303 <doannguyenhuyentrang9303@gmail.com>
2024-10-17 09:32:55 +0700

    update

--- app/src/main/java/com/prmproject/clothesstoremobileandroid/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/prmproject/clothesstoremobileandroid/ui/profile/ProfileViewModel.java
@@ -16,6 +16,6 @@ public class ProfileViewModel extends ViewModel {
     }
 
     public LiveData<ObjectResponse> getUserProfile() {
-        return userProfile;
+        return userRepository.getUser();
     }
 }
